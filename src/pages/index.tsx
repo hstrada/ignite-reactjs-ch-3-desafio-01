e2fe68705cc85e2c97fc348d951a4263864d52c9
hstrada <6079947+hstrada@users.noreply.github.com>
2022-04-18 13:56:16 -0300

    create css for global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
 import { GetStaticProps } from 'next';
+import { Head } from 'next/document';
+import Link from 'next/link';
+import { useState } from 'react';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { createClient, PreviewDataProps } from '../services/prismic';
 
@@ -25,10 +31,50 @@ interface HomeProps {
   previewData: PreviewDataProps;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }): JSX.Element {
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.hasNextPage);
+
   return (
     <>
-      <h1>hello</h1>
+      <div className={`${commonStyles.maxWidth} ${styles.container}`}>
+        <header className={styles.header}>
+          <img src="assets/logos/logo.svg" alt="logo" />
+        </header>
+
+        <main className={styles.content}>
+          <div className={styles.posts}>
+            {posts?.map(item => (
+              <Link key={item.uid} href={`post/${item.uid}`}>
+                <a className={styles.post}>
+                  <strong>{item.data.title}</strong>
+                  <p>{item.data.subtitle}</p>
+                  <div className={commonStyles.footerPostData}>
+                    <time>
+                      <FiCalendar />
+                      {item.first_publication_date}
+                    </time>
+                    <span>
+                      <FiUser />
+                      {item.data.author}
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            ))}
+          </div>
+
+          {nextPage && (
+            <button
+              type="button"
+              className={styles.button}
+              onClick={() => console.log('hello')}
+            >
+              Carregar mais posts
+            </button>
+          )}
+        </main>
+      </div>
     </>
   );
 }
@@ -37,9 +83,28 @@ export const getStaticProps: GetStaticProps = async ({ previewData }) => {
   const prismic = createClient({ previewData });
   const response = await prismic.getByType('posts', { pageSize: 2 });
 
-  console.log(response, 'response');
+  const posts = response.results.map(post => ({
+    uid: post.uid,
+    first_publication_date: format(
+      new Date(post.first_publication_date),
+      "dd MMM' 'yyyy",
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    },
+  }));
+
+  const postsPagination = {
+    results: posts,
+    hasNextPage: response.next_page,
+  };
 
   return {
-    props: {},
+    props: { postsPagination },
   };
 };
